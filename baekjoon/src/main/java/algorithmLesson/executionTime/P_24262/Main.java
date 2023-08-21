362f5bfd9f30b02600cc1dd48414a4fd0157c852
CodingDochi <cheers7129@gmail.com>
2023-08-22 00:00:00 +0900

    Updated file: Main.java

---
+++ baekjoon/src/main/java/algorithmLesson/executionTime/P_24262/Main.java
@@ -0,0 +1,19 @@
+package algorithmLesson.executionTime.P_24262;
+
+public class Main {
+
+/*
+아래 알고리즘의 수행시간을 구하는 문제이다.
+반복문이 없으므로 수행횟수는 1회이며 최고차항의 차수는 0이다.
+
+MenOfPassion(A[], n) {
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
+}
+ */
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
