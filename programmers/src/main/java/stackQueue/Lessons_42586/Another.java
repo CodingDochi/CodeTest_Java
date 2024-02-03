c419aea4cc71d1d56d441e304d011c70928007ac
CodingDochi <cheers7129@gmail.com>
2024-02-04 00:00:00 +0900

    Updated file: Another.java

---
+++ programmers/src/main/java/stackQueue/Lessons_42586/Another.java
@@ -0,0 +1,17 @@
+package stackQueue.Lessons_42586;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+class Another {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
+            }
+            dayOfend[day]++;
+        }
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+    }
+}
